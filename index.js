99585d9b8dcf239d286f00888f1a50500b9d49ce
Rob Ballantyne <raballantyne@gmail.com>
2019-06-03 16:53:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+ name=[];
+  return kittens.concat(name);
+}
+
+function prependKitten(name){
+  kittens = [name, ...kittens]
+  return kittens
+}
+
+function removeLastKitten(){
+  kittens = kittens.slice(-2);
+  return kittens;
+}
+
+function removeFirstKitten(){
+  kittens = kittens.slice(1);
+  return kittens;
+}
